d23f90e1f892b356be86f0303da41ddeafc220ed
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-07-07 14:24:02 -0400

    fixed auth options routing

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,5 @@
 import { getServerSession } from 'next-auth'
-
-import { authOptions } from '@app/api/auth/[...nextauth]/route'
+import { authOptions } from '@app/utils/authOptions'
 import { NextResponse } from 'next/server'
 
 export default async function getSession() {
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,5 @@
-import NextAuth, { AuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-
-import prisma from '@libs/prismadb'
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-}
+import NextAuth from 'next-auth'
+import { authOptions } from '@app/utils/authOptions'
 
 const handler = NextAuth(authOptions)
 
