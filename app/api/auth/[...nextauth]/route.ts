37094dd4753f514a96cce95262d9140bbb8c4330
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-07-07 13:25:09 -0400

    fixed nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 import prisma from '@libs/prismadb'
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
