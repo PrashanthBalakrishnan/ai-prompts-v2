fe6902c6cf12c1b5f7f708c46e7c6e414e4d9177
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-07-07 13:39:52 -0400

    fixed next auth issues

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,5 @@
-import bcrypt from 'bcrypt'
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-
-import prisma from '@libs/prismadb'
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-}
+import NextAuth from 'next-auth'
+import { authOptions } from '@utils/authOptions'
 
 const handler = NextAuth(authOptions)
 
