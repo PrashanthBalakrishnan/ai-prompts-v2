bf987e67dc897c4c00adce3a9072d9673da8a57e
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-07-07 14:26:03 -0400

    fixed get current user

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,13 +1,7 @@
 import { getServerSession } from 'next-auth'
+
 import { authOptions } from '@app/utils/authOptions'
-import { NextResponse } from 'next/server'
 
 export default async function getSession() {
-  const session = await getServerSession(authOptions)
-  if (!session) {
-    return new NextResponse(JSON.stringify({ error: 'unauthorized' }), {
-      status: 401,
-    })
-  }
-  return NextResponse.json({ authenticated: !!session })
+  return await getServerSession(authOptions)
 }
