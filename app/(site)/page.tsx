0b87e932b6adeb9014cdfde46706e7c8915e3223
PrashanthBalakrishnan <prashbala13@gmail.com>
2023-07-07 14:56:09 -0400

    added custom welcome name

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,18 @@
 import getAllPrompts from '@app/actions/getAllPrompts'
+import getCurrentUser from '@app/actions/getCurrentUser'
 import Feed from '@components/Feed'
 
 export default async function Home() {
+  const user = await getCurrentUser()
+
   const allPrompts = await getAllPrompts()
 
   return (
     <section>
       <section className="flex-center w-full flex-col">
+        <p className="desc">
+          Welcome Back <span className="font-semibold">{user?.name}</span>
+        </p>
         <h1 className="head_text black_gradient text-center">
           Unleash Your Creativity and Witness the Power of AI
           <br className="max-md:hidden" />
